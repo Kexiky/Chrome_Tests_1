5cac9ce6f96b9013bc71b52d57e80f76af7cd05b
Kexiky <51199842+Kexiky@users.noreply.github.com>
2019-07-14 14:54:02 +0300

    Задание 3.1 Прохождение по всем вкладкам МЕНЮ с помощью 3-х циклов

---
+++ Chrome_Zadanie_3_1.py
@@ -0,0 +1,46 @@
+import unittest
+import time
+from selenium import webdriver
+
+
+def Driver_get_tele2(dr):
+    dr.get("https://msk.tele2.ru/")
+    dr.find_element_by_class_name('menu-action').click()
+    time.sleep(2)
+    return dr.find_element_by_class_name('mobile-menu-modal')
+
+
+def driver_click_dir(m1_1, i):
+    m1_1.find_elements_by_xpath("//*[@class='main-mobile-menu']//a")[i].click()
+    return m1_1.find_elements_by_class_name('regular')
+
+
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+    def test_menu_tele2(self):
+        driver = self.driver
+
+        menu_1_1 = Driver_get_tele2(driver)
+
+        for i in range(4):
+            menu_list_1 = driver_click_dir(menu_1_1, i)
+
+            for j in range(len(menu_list_1)):
+                m1 = menu_list_1[j]
+                for k in range(len(m1.find_elements_by_tag_name('a'))):
+                    menu_list_1_1 = menu_list_1[j].find_elements_by_tag_name('a')
+                    menu_list_1_1[k].click()
+                    time.sleep(3)
+                    menu_1_1 = Driver_get_tele2(driver)
+                    menu_list_1 = driver_click_dir(menu_1_1, i)
+            menu_1_1 = Driver_get_tele2(driver)
+
+    def tearDown(self):
+        self.driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
